e583f7c088baa403f587cc701a70173137194962
Tuan Tuan LE <tlcong@linagora.com>
2019-12-26 11:44:30 +0700

    \#2 fix error when deleting an event which has attendee and alarm

--- lib/ITip/Broker.php
+++ lib/ITip/Broker.php
@@ -653,7 +653,7 @@ class Broker
                     'CN' => $attendee['name'],
                 ]);
 
-                if(isset($calendar->VEVENT->VALARM) && $calendar->VALARM->ACTION->getValue() == 'EMAIL') {
+                if (isset($calendar->VEVENT->VALARM) && 'EMAIL' === $calendar->VEVENT->VALARM->ACTION->getValue()) {
                     $event->add(clone $calendar->VEVENT->VALARM);
                     $event->VALARM->ATTENDEE->setValue($attendee['href']);
                 }
